5d3d5e1f137d332db58abdfe899ec24cc7758a4b
Richard Tung <zqdbyct@gmail.com>
2022-06-30 00:49:55 +0800

    feat: add constructor and getter/setter

--- src/model/Student.java
+++ src/model/Student.java
@@ -5,4 +5,43 @@ public class Student {
     private int age;
     private int gender;
     private String id_no;
+
+    public Student(String name, int age, int gender, String id_no) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.id_no = id_no;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getGender() {
+        return gender;
+    }
+
+    public void setGender(int gender) {
+        this.gender = gender;
+    }
+
+    public String getId_no() {
+        return id_no;
+    }
+
+    public void setId_no(String id_no) {
+        this.id_no = id_no;
+    }
 }
